fa9a5039abfcd2f6eca55294fd3984d20081039d
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-01-08 23:40:18 -0500

    Created SinglePatientView Scene and added navigation and functionality

--- TheraHaptics/Assets/Scripts/UI/DashBoardUIManager.cs
+++ TheraHaptics/Assets/Scripts/UI/DashBoardUIManager.cs
@@ -11,7 +11,15 @@ public class DashBoardUIManager : MonoBehaviour
     // Getting input fields
     [SerializeField] private TMP_InputField searchBox;
 
+
+    // Getting PopUp Box
+    [SerializeField] private GameObject personalInfoScreen;
     // Called by clicking My Dashboard; goes to dashboard scene
+
+    // Makes sure the PopUp isn't immediately visible
+    private void Awake(){
+        personalInfoScreen.SetActive(false);
+    }
     public void Dashboard(){
         SceneManager.LoadScene(1);
     }
@@ -28,7 +36,7 @@ public class DashBoardUIManager : MonoBehaviour
 
     // Called when Clicking Support; goes to support scene
     public void Support(){
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(5);
     }
 
     // Logs the user out
@@ -45,7 +53,8 @@ public class DashBoardUIManager : MonoBehaviour
     }
     
     public void ViewPatient(){
-        
+        // Check which patient here and save to staticdata
+        SceneManager.LoadScene(4);
     }
 
     public void OpenPatientChat(){
@@ -58,6 +67,13 @@ public class DashBoardUIManager : MonoBehaviour
 
     public void AddPatient(){
         
+    }
+    public void ShowPersonalInfo(){
+        personalInfoScreen.SetActive(true);
+    }
+
+    public void HidePersonalInfo(){
+        personalInfoScreen.SetActive(false);
     }
 
 }
