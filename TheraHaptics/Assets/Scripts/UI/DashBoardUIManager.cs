b54f49c0cbd0dbb55dc1adc3b924b9d0b265bdca
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-01-08 22:22:11 -0500

    Added other Physio Scenes and functionality to sidebar buttons

---
+++ TheraHaptics/Assets/Scripts/UI/DashBoardUIManager.cs
@@ -0,0 +1,39 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DashBoardUIManager : MonoBehaviour
+{
+    // Getting input fields
+    [SerializeField] private TMP_InputField searchBox;
+
+    // Called by clicking My Dashboard; goes to dashboard scene
+    public void Dashboard(){
+        SceneManager.LoadScene(1);
+    }
+
+    // Called by clicking My Account; goes to account scene
+    public void Account(){
+        SceneManager.LoadScene(2);
+    }
+
+    // Called by clicking My Patients; goes to patients scene
+    public void Patients(){
+        SceneManager.LoadScene(3);
+    }
+
+    // Called when Clicking Support; goes to support scene
+    public void Support(){
+        SceneManager.LoadScene(4);
+    }
+
+    // Logs the user out
+    public void LogOut(){
+        SceneManager.LoadScene(1);
+    }
+
+}
