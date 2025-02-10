5ec9d6adefce42159e7b6358ab01e8b2a4e65e80
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-02-10 04:43:57 -0500

    Finished TherapistModel
    Added Getters and Setters to TherapistModel Variables

--- TheraHaptics/Assets/Scripts/Therapist/TherapistModel.cs
+++ TheraHaptics/Assets/Scripts/Therapist/TherapistModel.cs
@@ -13,6 +13,58 @@ public class TherapistModel : MonoBehaviour
     private string lastName{ get; set; }
     private string email{ get; set; }
     private string productKeyID{ get; set; }
-    private List<string> AssignedPatients{ get; set; }
-    
+    private List<string> assignedPatients{ get; set; }
+
+    // Constructor
+    public TherapistModel(){
+
+    }
+
+    // Setter and Getter for therapistID
+    public void SetTherapistID(string therapistID) {
+        this.therapistID = therapistID;
+    }
+    public string GetTherapistID() {
+        return this.therapistID;
+    }
+
+    // Setter and Getter for firstName
+    public void SetFirstName(string firstName) {
+        this.firstName = firstName;
+    }
+    public string GetFirstName() {
+        return this.firstName;
+    }
+
+    // Setter and Getter for lastName
+    public void SetLastName(string lastName) {
+        this.lastName = lastName;
+    }
+    public string GetLastName() {
+        return this.lastName;
+    }
+
+    // Setter and Getter for email
+    public void SetEmail(string email) {
+        this.email = email;
+    }
+    public string GetEmail() {
+        return this.email;
+    }
+
+    // Setter and Getter for productKeyID
+    public void SetProductKeyID(string productKeyID) {
+        this.productKeyID = productKeyID;
+    }
+    public string GetProductKeyID() {
+        return this.productKeyID;
+    }
+
+    // Setter and Getter for assignedPatients
+    public void SetAssignedPatients(List<string> assignedPatients) {
+        this.assignedPatients = assignedPatients;
+    }
+    public List<string> GetAssignedPatients() {
+        return this.assignedPatients;
+    }
 }
