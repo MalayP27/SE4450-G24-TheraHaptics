145df299374d3dcf52b3f69b8636af847b47e2ad
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-02-10 04:05:42 -0500

    Added Sidebar Methods in PatientView

--- TheraHaptics/Assets/Scripts/Patient/PatientView.cs
+++ TheraHaptics/Assets/Scripts/Patient/PatientView.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class PatientView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Text Boxes
+    [SerializeField] private TMP_Text helloMessage;
+
+    // Constructor
+    public PatientView(){
+
+    }
+    // ==========Common Methods===========
+    // Method to update Main Welcome Message
+    public void setWelcomeMessage(String helloMessageReturn){
+        helloMessage.text = helloMessageReturn;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // Methods For SideBar Buttons
+    // Method for "My DashBoard" Button click
+    public void PatientDashboard(){
+        SceneManager.LoadScene("PatientDashboard");
+    }
+
+    // Method for "My Reports" Button click
+    public void PatientReports(){
+        SceneManager.LoadScene("PatientReports");
+    }
+
+    // Method for "My Account" Button click
+    public void PatientAccount(){
+        SceneManager.LoadScene("PatientAccount");
+    }
+
+    // Method for "Support" Button click
+    public void PatientSupport(){
+        SceneManager.LoadScene("Support");
+    }
+
+    // Method for "Log Out" Button click
+    public void PatientLogOut(){
+        SceneManager.LoadScene("SignIn");
     }
 }
