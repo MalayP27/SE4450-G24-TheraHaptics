f2b8b111db72e447ad920e3e1db023ccec71d06c
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-02-10 03:32:01 -0500

    Finished PatientModel
    Added Constructor and Getters and Setters

--- TheraHaptics/Assets/Scripts/Patient/PatientModel.cs
+++ TheraHaptics/Assets/Scripts/Patient/PatientModel.cs
@@ -20,4 +20,105 @@ public class PatientModel : MonoBehaviour
     private string dateJoined{ get; set; }
     private string goalID{ get; set; }
     private string therapistID{ get; set; }
+
+    // Constructor
+    public PatientModel(){
+
+    }
+
+    // Setter and Getter for patientID
+    public void SetPatientID(string patientID) {
+        this.patientID = patientID;
+    }
+    public string GetPatientID() {
+        return this.patientID;
+    }
+
+    // Setter and Getter for firstName
+    public void SetFirstName(string firstName) {
+        this.firstName = firstName;
+    }
+    public string GetFirstName() {
+        return this.firstName;
+    }
+
+    // Setter and Getter for lastName
+    public void SetLastName(string lastName) {
+        this.lastName = lastName;
+    }
+    public string GetLastName() {
+        return this.lastName;
+    }
+
+    // Setter and Getter for email
+    public void SetEmail(string email) {
+        this.email = email;
+    }
+    public string GetEmail() {
+        return this.email;
+    }
+
+    // Setter and Getter for phoneNumber
+    public void SetPhoneNumber(string phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public string GetPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    // Setter and Getter for address
+    public void SetAddress(string address) {
+        this.address = address;
+    }
+    public string GetAddress() {
+        return this.address;
+    }
+
+    // Setter and Getter for DOB
+    public void SetDOB(string DOB) {
+        this.DOB = DOB;
+    }
+    public string GetDOB() {
+        return this.DOB;
+    }
+
+    // Setter and Getter for gender
+    public void SetGender(string gender) {
+        this.gender = gender;
+    }
+    public string GetGender() {
+        return this.gender;
+    }
+
+    // Setter and Getter for diagnosis
+    public void SetDiagnosis(string diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+    public string GetDiagnosis() {
+        return this.diagnosis;
+    }
+
+    // Setter and Getter for dateJoined
+    public void SetDateJoined(string dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+    public string GetDateJoined() {
+        return this.dateJoined;
+    }
+
+    // Setter and Getter for goalID
+    public void SetGoalID(string goalID) {
+        this.goalID = goalID;
+    }
+    public string GetGoalID() {
+        return this.goalID;
+    }
+
+    // Setter and Getter for therapistID
+    public void SetTherapistID(string therapistID) {
+        this.therapistID = therapistID;
+    }
+    public string GetTherapistID() {
+        return this.therapistID;
+    }
 }
