059f723ba32d84eb8cccb0bbb8c8ed375f79623f
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-02-10 04:31:48 -0500

    Added Therapist Scripts
    TherapistModel
    TherapistView
    TherapistController

---
+++ TheraHaptics/Assets/Scripts/Therapist/TherapistModel.cs
@@ -0,0 +1,21 @@
+// Imports
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class Definition
+public class TherapistModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
