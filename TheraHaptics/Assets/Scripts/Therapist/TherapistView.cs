3bf8a534c6e1ce4c7b81b9fc6f31a2defe5a4cea
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-02-10 04:50:53 -0500

    Added Common Methods for TherapistView

--- TheraHaptics/Assets/Scripts/Therapist/TherapistView.cs
+++ TheraHaptics/Assets/Scripts/Therapist/TherapistView.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class TherapistView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Text Boxes
+    [SerializeField] private TMP_Text helloMessage;
+
+    // Constructor
+    public TherapistView(){
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // ==========Common Methods===========
+    // Method to update Main Welcome Message
+    public void setWelcomeMessage(String helloMessageReturn){
+        helloMessage.text = helloMessageReturn;
+    }
+    // Methods For SideBar Buttons
+    // Method for "My DashBoard" Button click
+    public void TherapistDashboard(){
+        SceneManager.LoadScene("TherapistDashboard");
+    }
+
+    // Method for "My Patients" Button click
+    public void TherapistPatients(){
+        SceneManager.LoadScene("TherapistPatients");
+    }
+
+    // Method for "My Account" Button click
+    public void TherapistAccount(){
+        SceneManager.LoadScene("TherapistAccount");
+    }
+
+    // Method for "Support" Button click
+    public void TherapistSupport(){
+        SceneManager.LoadScene("Support");
+    }
+
+    // Method for "Log Out" Button click
+    public void TherapistLogOut(){
+        SceneManager.LoadScene("SignIn");
     }
 }
