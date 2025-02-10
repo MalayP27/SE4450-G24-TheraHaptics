1c29ae6b7e27553ce9540fb1a1e42dcf7be1ce5e
Jan Awad <90866356+J0est4r123@users.noreply.github.com>
2025-02-10 03:29:21 -0500

    Added Patient MVC scripts
    PatientModel, PatientView, PatientController
    
    Added Variables for Patient

---
+++ TheraHaptics/Assets/Scripts/Patient/PatientModel.cs
@@ -0,0 +1,23 @@
+// Imports
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class Definition
+public class PatientModel : MonoBehaviour
+{
+    // Instantiating variables for the Login
+    private string patientID{ get; set; }
+    private string firstName{ get; set; }
+    private string lastName{ get; set; }
+    private string email{ get; set; }
+    private string phoneNumber{ get; set; }
+    private string address{ get; set; }
+    private string DOB{ get; set; }
+    private string gender{ get; set; }
+    private string diagnosis{ get; set; }
+    private string dateJoined{ get; set; }
+    private string goalID{ get; set; }
+    private string therapistID{ get; set; }
+}
