6b235d9080e6d5ac6c1b632a8ace9a7e31f6875b
Zaiyan Azeem <zaiyanazeem@gmail.com>
2025-03-18 21:34:33 -0400

    Login / Password Change

--- TheraHaptics/Assets/Scripts/_Patient/PatientController.cs
+++ TheraHaptics/Assets/Scripts/_Patient/PatientController.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using UnityEngine.Networking;
 
-public class PatientController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+    public class PatientController : MonoBehaviour
     {
-        
-    }
+        [System.Serializable]
+        public class PatientResponse
+        {
+            public string patientId;
+            public string firstName;
+            public string lastName;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static IEnumerator GetPatient()
+        {
+            string patientId = RegisterController.PatientId; // Ensure this is set correctly
+            string url = $"http://localhost:5089/api/patient/{patientId}";
+
+            using (UnityWebRequest request = UnityWebRequest.Get(url))
+            {
+                request.SetRequestHeader("Content-Type", "application/json");
+                request.SetRequestHeader("Accept", "application/json");
+
+                yield return request.SendWebRequest();
+
+                if (request.result == UnityWebRequest.Result.Success)
+                {
+                    string responseBody = request.downloadHandler.text;
+                    PatientResponse patient = JsonUtility.FromJson<PatientResponse>(responseBody);
+                    if (patient != null)
+                    {
+                        string fullName = patient.firstName + " " + patient.lastName;
+                        // Find the PatientView instance and update the welcome message
+                        PatientView patientView = FindObjectOfType<PatientView>();
+                        if(patientView != null)
+                            patientView.setWelcomeMessage(fullName);
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to parse patient information.");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Error fetching patient data: " + request.error);
+                }
+            }
+        }
     }
-}
